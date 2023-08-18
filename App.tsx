fc39297c08c52f5964cc9415ab73ad39aad8396c
Joshua Yee <joshuayee1001@gmail.com>
2023-08-17 17:35:11 -0700

    completed login functionality

--- App.tsx
+++ App.tsx
@@ -3,14 +3,43 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './app/screens/Login';
+import Home from './app/screens/Home';
+import { User, onAuthStateChanged } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import { FIREBASE_AUTH } from './FirebaseConfig';
+import Example from './app/screens/Example';
 
 
 const Stack = createNativeStackNavigator();
+
+const InsideStack = createNativeStackNavigator();
+
+function InsideLayout() {
+  return (
+    <InsideStack.Navigator>
+      <InsideStack.Screen name="Inside" component={Home} />
+      <InsideStack.Screen name="Example" component={Example} />
+    </InsideStack.Navigator>
+  )
+}
 export default function App() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      console.log('user', user);
+      setUser(user);
+    })
+  }, []);
+
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
+        {user ? (<Stack.Screen name="Inside" component={InsideLayout} options={{ headerShown: false }} />
+        ) : (
+          <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,6 +1,7 @@
-import { View, Text, StyleSheet, TextInput, Image } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Image, ActivityIndicator, Button, KeyboardAvoidingView } from 'react-native'
 import React, { useState } from 'react'
 import { FIREBASE_AUTH } from '../../FirebaseConfig';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -8,25 +9,69 @@ const Login = () => {
   const [loading, setLoading] = useState(false);
   const auth = FIREBASE_AUTH;
 
-  
+
+  const signIn = async () => {
+    setLoading(true);
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      console.log(response);
+    }
+    catch (error: any) {
+      console.log(error);
+      alert('Sign in failed: ' + error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const signUp = async () => {
+    setLoading(true);
+    try {
+      const response = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(response);
+      alert('Check your emails!');
+    }
+    catch (error: any) {
+      console.log(error);
+      alert('Sign in failed: ' + error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Image
         style={styles.logo}
-        source={{uri: 'https://i.ibb.co/frnmnHs/stylewithus-LOGO.jpg'}}
+        source={{ uri: 'https://i.ibb.co/frnmnHs/stylewithus-LOGO.jpg' }}
       />
-      
-      <TextInput value={email} style={styles.input}
-        placeholder="Email"
-        autoCapitalize="none"
-        onChangeText={ (text) => setEmail(text)}
-      ></TextInput>
+
+      <KeyboardAvoidingView behavior="padding">
+
+        <TextInput value={email} style={styles.input}
+          placeholder="Email"
+          autoCapitalize="none"
+          onChangeText={(text) => setEmail(text)}
+        ></TextInput>
         <TextInput value={password} style={styles.input}
-        secureTextEntry={true}
-        placeholder="Password"
-        autoCapitalize="none"
-        onChangeText={ (text) => setPassword(text)}
-      ></TextInput>
+          secureTextEntry={true}
+          placeholder="Password"
+          autoCapitalize="none"
+          onChangeText={(text) => setPassword(text)}
+        ></TextInput>
+
+        {loading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <>
+            <Button title="Login" onPress={signIn} />
+            <Button title="Create Account" onPress={signUp} />
+          </>
+        )}
+
+      </KeyboardAvoidingView>
+
+
     </View>
   )
 }
@@ -35,27 +80,27 @@ export default Login;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
+    flex: 1,
     justifyContent: 'flex-start',
     backgroundColor: '#8F6E5D',
     alignItems: 'center',
     //flexDirection: 'column'
   },
-  logo:{
+  logo: {
     width: 300,
     height: 300,
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 100,
-   
+
   },
   input: {
     marginHorizontal: 20,
     marginVertical: 4,
-    height: 50, 
+    height: 50,
     width: 250,
     borderWidth: 1,
-    borderRadius: 4, 
+    borderRadius: 4,
     padding: 10,
     backgroundColor: '#fff'
   }
